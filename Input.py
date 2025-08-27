11b533b20e2bae1c15c48aa6c58d865ef282917c
CASSIBRA000 <cassibra000@warren.k12.in.us>
2025-08-27 08:43:25 -0400

    Initial commit

---
+++ Input.py
@@ -0,0 +1,8 @@
+print("=== Temperature Converter ===")
+print("Convert Celsius to Fahrenheit")
+print()
+C = int(input("Tempature in Celsius: "))
+F = C * 9 / 5 + 32
+print("Tempature in Farenheit:", F)
+print()
+print("Thank you for using the Temperature Converter!")
